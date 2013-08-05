a067cb8e80bf8006095085bccaf5fdd5cc099633
Jessica Palay <jessica.c.palay@gmail.com>
2013-08-05 13:26:14 -0500

    Troubleshooting email prodcution error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,4 @@ Veggieout::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'jessicapalayveggieout.herokuapp.com' }  
 end
-end
+
