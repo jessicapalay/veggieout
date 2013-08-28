a1884bb9c8da7dbcd8c75928cedaba54a93c7b0b
Jessica Palay <jessica.c.palay@gmail.com>
2013-08-28 13:53:41 -0500

    added new css

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [90, 90]
+  process :resize_to_fill => [300, 300]
   #
   # def scale(width, height)
   #   # do something
